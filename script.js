f91722bc751001f82b968af61cc8c4781a4d88d9
tarannumsaxena <73153067+tarannumsaxena@users.noreply.github.com>
2020-12-06 22:24:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ window.onclick = function(event) {
   }
 }
 function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
   }
+}
